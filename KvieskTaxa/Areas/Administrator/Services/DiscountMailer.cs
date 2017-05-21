6006b97902cfe70903b3cd343a82b8719e22d428
Aivaras Baranauskas <aiarasb@gmail.com>
2017-05-21 22:24:09 +0300

    admin area

--- KvieskTaxa/Areas/Administrator/Services/DiscountMailer.cs
+++ KvieskTaxa/Areas/Administrator/Services/DiscountMailer.cs
@@ -3,11 +3,7 @@
  */
 using KvieskTaxa.Database.Models;
 using KvieskTaxa.Database;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace KvieskTaxa.Areas.Administrator.Services
 {
