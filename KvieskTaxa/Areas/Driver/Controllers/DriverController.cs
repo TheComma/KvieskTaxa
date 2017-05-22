75d92490cdc8d1a27a8c1b65bc5ecd1ab81d2f96
Paulinik <paulius.pat@gmail.com>
2017-05-22 23:16:35 +0300

    edits

--- KvieskTaxa/Areas/Driver/Controllers/DriverController.cs
+++ KvieskTaxa/Areas/Driver/Controllers/DriverController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KvieskTaxa.Database;
 
 namespace KvieskTaxa.Areas.Driver.Controllers
 {
     public class DriverController : Controller
     {
+        private DataModelContext dbContext;
         public ActionResult showCalls()
         {
-            return View();
+            return View(dbContext.Calls.ToList());
         }
         public void openCallInfo()
         {
